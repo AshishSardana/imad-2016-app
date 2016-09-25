ef5628f54594b23cd6527348f8f81c0f0d277b99
AshishSardana <ashishsardana21@gmail.com>
2016-09-25 18:27:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,10 @@
 console.log('Loaded!');
 
 //Change the text of the main-text division
-var element = document.getELementById('main-text');
-element.innerHtml = 'New Value';
+var element = document.getElementById('main-text');
+element.innerHTML = 'New Value that is generated through javascript.';
+
+var img = document.getElementById('img');
+img.onclick = function() {
+    img.style.marginLeft = 100px;
+}
