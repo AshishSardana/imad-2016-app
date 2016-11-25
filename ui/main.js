39f1dc39757bbe8f9b86566a293837e65f6f06a9
Ashish Sardana <ashishsardana21@gmail.com>
2016-11-25 23:55:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -44,6 +44,41 @@ button.onclick = function(){
     
 };
 
+//Submit username/password to login
+var submit1 = document.getElementById('submit_btn');
+
+submit1.onclick = function(){
+	// Create a request
+	var request = new XMLHttpRequest();
+	//Capture the response and store it in a variable
+    request.onreadystatechange = function() {
+		request.onreadystatechange = function () {
+			if(request.readyState === XMLHttpRequest.DONE){
+				if(request.status === 200){
+					console.log('user logged in');
+					alert('Logged in successfully');
+				}
+				else if(request.status === 403){
+				    alert('Username/password is incorrect.');
+				}
+				else if(request.status === 500){
+				    alert('Something went wrong on the server');
+				}
+			}
+			//Not done yet
+		}
+	}
+	
+    //Make the request
+	var username = document.getElementById('username').value;
+	var password = document.getElementById('password').value;
+	console.log(username);
+	console.log(password);
+	request.open('POST', 'http://ashishsardana.imad.hasura-app.io/login', true);
+	request.setRequestHeader('Content-Type', 'application/json');
+	request.send(JSON.stringify({username: username, password: password}));
+};
+
 //Submit Name
 var submit = document.getElementById('submit_button');
 
