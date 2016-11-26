dbbaddb0cdab1f7e62277439e5b5f52e34afdfd9
Ashish Sardana <ashishsardana21@gmail.com>
2016-11-26 15:25:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -200,6 +200,10 @@ app.get('check-login', function(req, res){
     }
 });
 
+app.get('logout', function(req,res){
+    delete req.session.auth;
+    res.send('You are logged out.');
+});
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
